3e881a97d0af9daa4550cb18a2d599adb9628c0f
dhruvil <dd.droid@yahoo.com>
2016-08-11 15:48:06 +0530

    Initial commit

---
+++ app/src/main/java/com/webmne/salestracker/LauncherActivity.java
@@ -0,0 +1,16 @@
+package com.webmne.salestracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+
+
+    }
+}
